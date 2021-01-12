ef4da046e3b448ba9cc85c8d1f660bf259b58318
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-12 13:21:29 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -3,11 +3,11 @@ import React, {Component} from 'react';
 
 export default class CoordinatesButton extends Component {
   
-  return (
-    <form>
-    <button> </button>
-    
-  </form>
-    )
   
+  
+  render(){
+  return (
+    <button> </button>
+    )
+  }
 }
